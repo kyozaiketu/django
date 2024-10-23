0b17f7f952f6135f1ef09c9b17ece70c437ce73b
kyozaiketu <kyozkt_186@outlook.jp>
2024-10-23 14:38:15 +0900

    buildfile change5

--- build.sh
+++ build.sh
@@ -2,7 +2,14 @@
 # exit on error
 set -o errexit
 
+# 安装依赖
 pip install -r requirements.txt
+
+# 收集静态文件
 python manage.py collectstatic --no-input
+
+# 运行数据库迁移
 python manage.py migrate
+
+
 python manage.py create_superuser
