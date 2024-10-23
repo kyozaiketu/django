20b43dbdc2c594c95b1b6a6ae4e89c8d721526ff
kyozaiketu <kyozkt_186@outlook.jp>
2024-10-23 15:51:24 +0900

    buildfile change7

--- build.sh
+++ build.sh
@@ -2,17 +2,22 @@
 # 出错时退出
 set -o errexit
 
-# 安装依赖
+echo "正在安装依赖..."
 pip install -r requirements.txt
+echo "依赖安装完成"
 
-# 收集静态文件
+echo "正在收集静态文件..."
 python manage.py collectstatic --no-input
+echo "静态文件收集完成"
 
-# 运行数据库迁移
+echo "正在应用数据库迁移..."
 python manage.py migrate
+echo "数据库迁移完成"
 
-# 创建超级用户
+
 python manage.py create_superuser
 
-# 启动 Gunicorn 服务器并指定 UvicornWorker 和端口
+echo "正在启动 Gunicorn..."
 gunicorn mysite.asgi:application -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT
+echo "Gunicorn 启动完成"
+
