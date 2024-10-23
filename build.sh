266a2750aee5f930f06e551634bab3c4f1a51585
kyozaiketu <kyozkt_186@outlook.jp>
2024-10-23 14:25:43 +0900

    builfile change4

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
-# Exit on error
+#!/usr/bin/env bash
+# exit on error
 set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py create_superuser 
+python manage.py create_superuser
